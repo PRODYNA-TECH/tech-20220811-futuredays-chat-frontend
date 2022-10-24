a20b6535b5214808a83ce552779ca960b0f9e2b2
Christian Häusler <ch.haeusler@gmail.com>
2022-10-24 10:04:44 +0200

    feat: api client generator tooling

---
+++ api-client/models/index.ts
@@ -0,0 +1,8 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './Chat';
+export * from './ChatCreate';
+export * from './Message';
+export * from './MessageCreate';
+export * from './User';
+export * from './UserCreate';
